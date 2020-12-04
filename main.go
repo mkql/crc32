bdc4273e32f4b85a730eab757f9718a47d755fdb
Masahiro Koizumi <masahiro.koizumi@qlonolink.com>
2020-12-04 10:50:35 +0900

    Accept multiple file names

--- main.go
+++ main.go
@@ -9,8 +9,9 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 3 {
-		log.Printf("exactly two arg needed, got %v", os.Args)
+	if len(os.Args) < 3 {
+		log.Printf("two or more args needed, got %v\n", os.Args)
+		log.Println("Usage: crc32 -c/-k/-i file1 file2 ...")
 		os.Exit(1)
 	}
 	var polynomial uint32
@@ -25,30 +26,28 @@ func main() {
 		log.Println("wrong arg")
 		os.Exit(1)
 	}
-	h, err := hashFileCrc32(os.Args[2], polynomial)
-	if err != nil {
-		panic(err)
+	for _, file := range os.Args[2:] {
+		h, err := hashFileCrc32(file, polynomial)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Print(h)
 	}
-	fmt.Print(h)
 }
 func hashFileCrc32(filePath string, polynomial uint32) (string, error) {
-	//Initialize an empty return string now in case an error has to be returned
-	var returnCRC32String string
-	//Open the fhe file located at the given path and check for errors
+	var result string
+
 	file, err := os.Open(filePath)
 	if err != nil {
-		return returnCRC32String, err
+		return result, err
 	}
-	//Tell the program to close the file when the function returns
 	defer file.Close()
-	//Create the table with the given polynomial
-	tablePolynomial := crc32.MakeTable(polynomial)
-	//Open a new hash interface to write the file to
-	hash := crc32.New(tablePolynomial)
-	//Copy the file in the interface
-	if _, err := io.Copy(hash, file); err != nil {
-		return returnCRC32String, err
-	}
-	return fmt.Sprintln("%08X", hash.Sum32()), nil
-}
 
+	tablePolynomial := crc32.MakeTable(polynomial)
+	hash := crc32.New(tablePolynomial)
+	if _, err := io.Copy(hash, file); err != nil {
+		return result, err
+	}
+
+	return fmt.Sprintf("%08X", hash.Sum32()), nil
+}
